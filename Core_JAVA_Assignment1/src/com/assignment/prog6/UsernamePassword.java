1a5f794f81ec7c35b313ee6a287f1cf77b6a9534
catalyst88 <bhadanevaibhav88@gmail.com>
2020-12-07 09:25:50 -0500

    # WARNING: head commit changed in the meantime
    Assignment1 first commit

--- Core_JAVA_Assignment1/src/com/assignment/prog6/UsernamePassword.java
+++ Core_JAVA_Assignment1/src/com/assignment/prog6/UsernamePassword.java
@@ -1,8 +1,9 @@
 package com.assignment.prog6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class UsernamePassword {
+public class UsernamePassword   {
 
 	
 	public static String strUsername = "vbhadane";
@@ -20,17 +21,55 @@ public class UsernamePassword {
 		 */
 		
 		 for (int i=0;i<3;i++) { 
-			 System.out.println("Please enter your username ");
+			 System.out.println("Please enter your username: \n");
 				Scanner sc= new Scanner(System.in);
 				String enteredUsr=sc.nextLine().toString();
 			 if(strUsername.equals(enteredUsr)) {
 				 i+=3;
-				}else {
-					System.out.println("try again!!");
+				 /*
+				  * Password match here
+				  */
+				
+				 
+				 for (int p=0; p<3; p++) {
+					System.out.println("Please Enter password : \n");
+					Scanner sc1= new Scanner(System.in);
+					String enteredPass= sc1.nextLine().toString();
+					
+					if(strPassword.equals(enteredPass)) {
+						p+=3;
+					}
+					else 
+					{
+						if(p==2) {
+							throw new InputMismatchException("Password max attempt reached!!");
+						}
+						else 
+							System.out.println("try again!!");
+					}
+					
+					
+					
+				 }
+				 
+				 
+				 
+				}else 
+				{
+					if(i==2) {
+						throw new InputMismatchException("Username max attempt reached!!");
+					}
+					else 
+						System.out.println("try again!!");
 				}
 			 
 		 }
 		 
+	}
+
+	private static Exception LoginExecetion() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
